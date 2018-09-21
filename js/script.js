2b3e2b33be315b36777cb280ca4f75c8bdd9f8b2
Vincent2605 <vh.1995.inquiry@gmail.com>
2018-09-21 21:46:14 +0200

    final result challenge w3

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ function switchChannel(ChannelName) {
     console.log('Turning into Channel ' + ChannelName);
     $('#rightside .app-bar span').html(ChannelName);
     $('#rightside .app-bar strong').html('<a href="https://map.what3words.com/upgrading.never.helps">upgrading.never.helps</a>');
+    $('#channels ul li').removeClass('selected-channel');
+    $('li:contains('+ChannelName+')').addClass('selected-channel');
 }
 function dislikeChannel() {
     $('#rightside .app-bar img').attr("src","https://ip.lfe.mw.tum.de/sections/star-o.png");
@@ -10,7 +12,21 @@ function dislikeChannel() {
 function likeChannel() {
     $('#rightside .app-bar img').attr("src","https://ip.lfe.mw.tum.de/sections/star.png");
 } 
-function selectTab() {
+function selectTab1() {
     $('#tab-bar button').removeClass('selected-tab');
-    $('#tab-bar button').addClass('selected-tab');
+    $('#new-button').addClass('selected-tab');
+    console.log('Changing to tab New');
+}
+function selectTab2() {
+    $('#tab-bar button').removeClass('selected-tab');
+    $('#trending-button').addClass('selected-tab');
+    console.log('Changing to tab Trending');
+}
+function selectTab3() {
+    $('#tab-bar button').removeClass('selected-tab');
+    $('#favorites-button').addClass('selected-tab');
+    console.log('Changing to tab Favorites');
+}
+function showEmojis()  {
+    $('#emojis').toggle();
 }
